e352d5ea113a8339ead294b9f2e6470f050235fd
vidushij11 <vidushij078@gmail.com>
2024-09-16 04:49:11 +0530

    updated bg

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import bgImg from "../images/bg.jpg";
 
 export default function Login() {
+  const divStyle = {
+    backgroundImage: `url(${bgImg})`,
+  };
   return (
-    <div className="bg-my-image bg-cover bg-center h-screen flex justify-center md:justify-start items-center ">
+    <div
+      style={divStyle}
+      className="bg-cover bg-center h-screen flex justify-center md:justify-start  items-center "
+    >
       <div className="flex flex-col justify-center items-center w-fit rounded-lg bg-white md:bg-transparent px-10 py-6">
         <h1 className="text-2xl text-center font-bold mb-16 ">Login</h1>
         <input
